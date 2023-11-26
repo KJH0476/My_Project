179989b724513938f3e670f982639f59e403be0a
JinHyeok <hyeok1234565@gamail.com>
2023-11-26 17:04:36 +0900

    불필요 코드 수정

--- src/main/java/hello/myproject/web/user/UserController.java
+++ src/main/java/hello/myproject/web/user/UserController.java
@@ -2,7 +2,6 @@ package hello.myproject.web.user;
 
 import hello.myproject.domain.user.User;
 import hello.myproject.domain.user.UserRepository;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
