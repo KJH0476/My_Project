8937f3d6eaa05face6e3eb93bdac9867315a65f5
JinHyeok <hyeok1234565@gamail.com>
2023-12-02 18:30:05 +0900

    댓글 작성 및 보여주는 로직 수정, 게시글과 댓글 삭제, 게시글 수정 컨트롤러 추가

--- src/main/java/hello/myproject/web/board/BoardController.java
+++ src/main/java/hello/myproject/web/board/BoardController.java
@@ -3,7 +3,7 @@ package hello.myproject.web.board;
 import hello.myproject.domain.board.Board;
 import hello.myproject.domain.board.BoardService;
 import hello.myproject.domain.comment.Comment;
-import hello.myproject.domain.comment.CommentService;
+import hello.myproject.domain.comment.CommentRepository;
 import hello.myproject.domain.user.User;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +13,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Slf4j
@@ -21,9 +23,9 @@ import java.util.List;
 public class BoardController {
 
     private final BoardService boardService;
-    private final CommentService commentService;
+    private final CommentRepository commentRepository;
 
-    //글쓰기 양식
+    //글쓰기 폼
     @GetMapping("/board-form")
     public String boardForm(){
         return "board/boardForm";
@@ -35,6 +37,32 @@ public class BoardController {
         log.info("run BoardController Save");
         boardService.saveBoard(board);
         response.getWriter().write("ok");
+    }
+
+    //게시글 수정폼
+    @GetMapping("/board-edit/{id}")
+    public String editBoardForm(@PathVariable String id, Model model){
+        Board board = boardService.findByBoardId(id);
+        model.addAttribute("board", board);
+
+        return "/edit/editBoardForm";
+    }
+
+    //게시글 수정
+    @PostMapping("/board-edit/{id}")
+    public void editBoard(@RequestBody Board board, @PathVariable String id, HttpServletResponse response) throws IOException {
+        log.info("edit BoardController");
+        boardService.updateBoardService(id, board);
+
+        response.getWriter().write("ok");
+    }
+
+    @GetMapping("/board-delete/{id}")
+    public String deleteBoard(@PathVariable String id){
+        log.info("deleteBoard");
+        boardService.deleteBoardService(id);
+
+        return "page/myPage";
     }
 
     //마이페이지
@@ -72,21 +100,40 @@ public class BoardController {
         return "page/menuPage";
     }
 
+    //게시글, 댓글 보여주기
     @GetMapping("/board/{id}")
     public String boardView(@PathVariable String id, Model model){
         Board board = boardService.findByBoardId(id);
-        List<Comment> comments = commentService.commentByBoardId(id);
+        List<Comment> comments = commentRepository.findByBoardId(Long.parseLong(id));
+
         board.setCommentCount(comments.size());
         log.info("boardView={}", board);
         model.addAttribute("board", board);
+
+        log.info("comments={}", comments);
         if(comments != null) model.addAttribute("comments", comments);
         else model.addAttribute("comments", null);
         return "board/boardView";
     }
 
+    //댓글 작성
     @PostMapping("/board/{id}")
-    public String writeComment(@ModelAttribute Comment comment, @PathVariable String id){
-        commentService.saveComment(comment);
+    public String saveComment(@ModelAttribute Comment comment, @PathVariable String id){
+        log.info("saveComment");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("YYYY-MM-dd E HH:mm");
+        String formattedDateTime = LocalDateTime.now().format(formatter);
+
+        comment.setTimeStamp(formattedDateTime);
+
+        commentRepository.save(comment);
         return "redirect:/board/"+id;
+    }
+
+    //댓글 삭제
+    @GetMapping("/comment/delete/{boardId}/{commentId}")
+    public String removeComment(@PathVariable String boardId, @PathVariable("commentId") String id){
+        commentRepository.deleteComment(Long.parseLong(id));
+
+        return "redirect:/board/"+Long.parseLong(boardId);
     }
 }
